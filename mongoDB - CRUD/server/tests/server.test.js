62379a4b186a6dab71b57a51d1713ad11f46f7e5
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-20 20:57:57 -0800

    add PATCH /todos/:id

--- mongoDB - CRUD/server/tests/server.test.js
+++ mongoDB - CRUD/server/tests/server.test.js
@@ -14,7 +14,9 @@ const todos = [
   },
   {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
   }
 ];
 
@@ -144,6 +146,46 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete('/todos/123abc')
       .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', done => {
+    const id = todos[0]._id.toHexString();
+    const text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.text).toBe(text);
+        expect(res.body.data.completed).toBe(true);
+        expect(typeof res.body.data.completedAt).toBe('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', done => {
+    const id = todos[1]._id.toHexString();
+    const text = 'New text';
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.text).toBe(text);
+        expect(res.body.data.completed).toBe(false);
+        expect(res.body.data.completedAt).toBeFalsy();
+      })
       .end(done);
   });
 });
