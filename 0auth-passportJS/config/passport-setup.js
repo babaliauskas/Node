8a6480f0f27c50a57f2a86e2124d415f3f518e5e
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2019-01-05 16:31:37 -0800

    Add google+ strategy

---
+++ 0auth-passportJS/config/passport-setup.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys')
+
+passport.use(
+  new GoogleStrategy({
+    // options fot the google strategy
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+  }),
+  () => {
+    // passport callback function
+  }
+);
