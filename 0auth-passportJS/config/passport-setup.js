9145fb33d7454f8b77f49b7cc4943503d8a9d904
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2019-01-05 17:25:45 -0800

    add mongoDB - mlab

--- 0auth-passportJS/config/passport-setup.js
+++ 0auth-passportJS/config/passport-setup.js
@@ -5,10 +5,13 @@ const keys = require('./keys')
 passport.use(
   new GoogleStrategy({
     // options fot the google strategy
+    callbackURL:'/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-  }),
-  () => {
+  },
+  (accessToken, refreshToken, profile, done) => {
     // passport callback function
-  }
+    console.log('passport')
+    console.log(profile)
+  })
 );
