4a692bd3bbb93e7e79e729eab0406deeba1b1a25
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2019-01-05 19:12:03 -0800

    Add cookies

--- 0auth-passportJS/config/passport-setup.js
+++ 0auth-passportJS/config/passport-setup.js
@@ -3,6 +3,17 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys')
 const User = require('../models/user-model')
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+})
+
 passport.use(
   new GoogleStrategy({
     // options fot the google strategy
@@ -18,6 +29,7 @@ passport.use(
       if (currentUser) {
         // already have the user
         console.log('user is: ', currentUser)
+        done(null, currentUser)
       } else {
         // if not, create user in our db
         new User({
@@ -25,6 +37,7 @@ passport.use(
           googleId: profile.id
         }).save().then(newUser => {
           console.log('new user created: ', newUser)
+          done(null, newUser)
         })
       }
     })
